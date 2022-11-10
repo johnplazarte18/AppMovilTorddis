99ba009ab4891a787b205e00a663b31c7ce95fa8
johnplazarte18 <john.plazarte2017@uteq.edu.ec>
2022-11-10 08:15:57 -0500

    ui login

--- app/src/main/java/com/example/torddis/MainActivity.java
+++ app/src/main/java/com/example/torddis/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.torddis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+     public void ocIniciarSesion(View view){
+         Intent intent = new Intent(this, Iniciar_sesion.class);
+         startActivity(intent);
+     }
 }
