fd2a6fb0b6eea18ba78c46516fb24724c40aefe1
johnplazarte18 <john.plazarte2017@uteq.edu.ec>
2022-11-19 23:37:48 -0500

    Menu de opciones

--- app/src/main/java/com/example/torddis/Iniciar_sesion.java
+++ app/src/main/java/com/example/torddis/Iniciar_sesion.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
--- app/src/main/java/com/example/torddis/Menu_opciones.java
+++ app/src/main/java/com/example/torddis/Menu_opciones.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.torddis.models.Tutor;
+
 public class Menu_opciones extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
+        getSupportActionBar().hide();
     }
 }
