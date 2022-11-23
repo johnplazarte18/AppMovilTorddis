136abf61f911602960974357da9da182a2613d0f
johnplazarte18 <john.plazarte2017@uteq.edu.ec>
2022-11-23 13:13:05 -0500

    Menu lateral y efecto de seleccion de opciones

--- app/src/main/java/com/example/torddis/Menu_opciones.java
+++ app/src/main/java/com/example/torddis/Menu_opciones.java
@@ -1,20 +1,30 @@
 package com.example.torddis;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.torddis.models.Tutor;
+import com.google.android.material.navigation.NavigationView;
 
-public class Menu_opciones extends AppCompatActivity {
-
+public class Menu_opciones extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    NavigationView navView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_opciones);
         getSupportActionBar().hide();
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
     }
     public void ocSupervisados(View view){
         Intent intent = new Intent(getApplicationContext(), ActSupervisados.class);
@@ -39,5 +49,13 @@ public class Menu_opciones extends AppCompatActivity {
     public void ocReportes(View view){
         Intent intent = new Intent(getApplicationContext(), ActReportes.class);
         startActivity(intent);
+    }
+    public void onOptionsItemSelected(View view) {
+                drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(GravityCompat.START);
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
